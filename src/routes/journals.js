4d6b559371491d1ad0ba3b3627569d4976edb938
amos chan <amoschan1@gmail.com>
2024-02-02 19:19:31 +0800

    add new paths for journal get and post requests

--- src/routes/journals.js
+++ src/routes/journals.js
@@ -24,4 +24,112 @@ journalsRouter.get('/get-journal-available', authenticateToken, async (req, res)
     }
 });
 
+//get journal entry
+journalsRouter.get('/get-journal-entry', authenticateToken, async (req, res) => {
+    const { user_id } = req.user;
+    const { date } = req.query;
+
+    try {
+        //check if the current date has a taskday entry
+        let taskdayQuery = 'SELECT id FROM taskdays WHERE user_id = ? AND date = ?';
+        database.query(taskdayQuery, [user_id, date], (taskdayErr, taskdayResult) => {
+            if (taskdayErr) {
+                handleServerError(res, taskdayErr);
+            } else {
+                if (taskdayResult.length > 0) {
+                    //if taskday entry exists, use its id to get journal
+                    const taskdayId = taskdayResult[0].id;
+                    let journalQuery = 'SELECT * FROM journals WHERE taskdays_id = ?';
+                    database.query(journalQuery, [taskdayId], (journalErr, journalResult) => {
+                        if (journalErr) {
+                            handleServerError(res, journalErr);
+                        } else {
+                            res.status(200).json({ result: journalResult });
+                        }
+                    });
+                } else {
+                    //if taskday entry does not exist, create a new entry and continue with the same query
+                    let createTaskdayQuery = 'INSERT INTO taskdays (user_id, date) VALUES (?, ?)';
+                    database.query(createTaskdayQuery, [user_id, date], (createTaskdayErr, createTaskdayResult) => {
+                        if (createTaskdayErr) {
+                            handleServerError(res, createTaskdayErr);
+                        } else {
+                            //use the newly created taskday id get journal
+                            const newTaskdayId = createTaskdayResult.insertId;
+                            let journalQuery = 'SELECT * FROM tasks WHERE taskdays_id = ?';
+                            database.query(journalQuery, [newTaskdayId], (journalErr, journalResult) => {
+                                if (journalErr) {
+                                    handleServerError(res, journalErr);
+                                } else {
+                                    res.status(200).json({ result: journalResult });
+                                }
+                            });
+                        }
+                    });
+                }
+            }
+        });
+    } catch (err) {
+        handleServerError(res, err);
+    }
+});
+
+//update || add journal entry
+/* 
+get taskdays id -> get journals id
+if journals id -> update journals query
+if not journals id -> add journals query
+*/
+journalsRouter.post('/update-journal-entry', authenticateToken, async (req, res) => {
+    const { user_id } = req.user;
+    const { date, gratitude, general, moodDescription, mood } = req.body;
+
+    try {
+        //check if the current date has a taskday entry
+        let taskdayQuery = 'SELECT id FROM taskdays WHERE user_id = ? AND date = ?';
+        database.query(taskdayQuery, [user_id, date], (taskdayErr, taskdayResult) => {
+            if (taskdayErr) {
+                handleServerError(res, taskdayErr);
+            } else {
+                if (taskdayResult.length > 0) {
+                    //use taskday id to check if journal exists 
+                    const taskdayId = taskdayResult[0].id;
+                    console.log(taskdayId)
+                    let journalQuery = 'SELECT * FROM journals WHERE taskdays_id = ?';
+                    database.query(journalQuery, [taskdayId], (journalErr, journalResult) => {
+                        if (journalErr) {
+                            handleServerError(res, journalErr);
+                        } else {
+                            if (journalResult.length > 0) {
+                                //update journal entry
+                                const journalId = journalResult[0].id;
+                                let updateJournalQuery = 'UPDATE journals SET gratitude = ?, general = ?, mood = ?, overallmood = ? WHERE id = ?';
+                                database.query(updateJournalQuery, [gratitude, general, moodDescription, mood, journalId], (updateJournalErr, updateJournalResult) => {
+                                    if (updateJournalErr) {
+                                        handleServerError(res, updateJournalErr);
+                                    } else {
+                                        res.status(200).json({ result: updateJournalResult});
+                                    }
+                                });
+                            } else {
+                                //create new journal entry
+                                let addJournalQuery = 'INSERT INTO journals (gratitude, general, mood, overallmood, taskdays_id) VALUES (?, ?, ?, ?, ?)';
+                                database.query(addJournalQuery, [gratitude, general, moodDescription, mood, taskdayId], (addJournalErr, addJournalResult) => {
+                                    if (addJournalErr) {
+                                        handleServerError(res, addJournalErr);
+                                    } else {
+                                        res.status(200).json({ result: addJournalResult});
+                                    }
+                                });
+                            }
+                        }
+                    });
+                } 
+            }
+        });
+    } catch (err) {
+        handleServerError(res, err);
+    }
+});
+
 module.exports = journalsRouter;
